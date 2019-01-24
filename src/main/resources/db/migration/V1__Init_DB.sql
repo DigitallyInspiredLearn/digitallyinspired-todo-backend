a221f0ae384b3c7e57c559a1321cef932a48deff
Gearty <gearty@rambler.ru>
2019-01-24 22:42:05 +0200

    Added flyway migrations

---
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -0,0 +1,26 @@
+create table base_entity (id bigint not null, primary key (id)) engine=MyISAM;
+create table hibernate_sequence (next_val bigint) engine=MyISAM;
+insert into hibernate_sequence values ( 1 );
+create table task (
+    id bigint not null,
+    body varchar(255) not null,
+    is_complete bit not null,
+    todolist_id bigint,
+    primary key (id)) engine=MyISAM;
+
+create table todo_list (
+    id bigint not null,
+    todo_list_name varchar(100) not null,
+    user_owner_id bigint not null,
+    primary key (id)) engine=MyISAM;
+
+create table users (
+    id bigint not null,
+    email varchar(40),
+    name varchar(40),
+    password varchar(100),
+    role integer,
+    username varchar(30),
+    primary key (id)) engine=MyISAM;
+
+alter table task add constraint todolist_id_fk foreign key (todolist_id) references todo_list (id);
